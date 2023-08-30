789ad39a1a76f5000d38cd70ad8f3fa6b9d880c7
ishajais <126967483+ishajais@users.noreply.github.com>
2023-08-30 12:18:51 +0530

    recording.py

--- recording.py
+++ recording.py
@@ -1,86 +1,32 @@
 import cv2
-import keyboard
 import datetime
 
-def select_camera():
-    num_cameras = 0
-    for i in range(10):
-        cap = cv2.VideoCapture(i)
-        if cap.isOpened():
-            num_cameras += 1
-            cap.release()
-        else:
-            break
+cap = cv2.VideoCapture(0)
 
-    if num_cameras == 0:
-        print("No cameras found.")
-        return None
+fourcc = cv2.VideoWriter_fourcc(*'XVID')  
+output_file = 'recorded_video.avi'
+fps = 30.0  
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+out = cv2.VideoWriter(output_file, fourcc, fps, (frame_width, frame_height))
 
-    print("Available cameras:")
-    for i in range(num_cameras):
-        print(f"{i + 1}. Camera {i + 1}")
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
 
-    while True:
-        try:
-            camera_choice = int(input("Select the camera number (1, 2, ...): "))
-            if 1 <= camera_choice <= num_cameras:
-                return camera_choice - 1
-            else:
-                print("Invalid camera choice. Please try again.")
-        except ValueError:
-            print("Invalid input. Please enter a number.")
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-def record_video(output_file, camera_index, fps=20.0, duration=10):
-    cap = cv2.VideoCapture(camera_index)
+    cv2.putText(frame, current_time, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    if not cap.isOpened():
-        print(f"Error: Camera {camera_index + 1} could not be opened.")
-        return
+    cv2.imshow('Surveillance', frame)
 
-    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    out.write(frame)
 
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(output_file, fourcc, fps, (frame_width, frame_height))
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-    start_time = datetime.datetime.now()
-    while (datetime.datetime.now() - start_time).seconds < duration:
-        ret, frame = cap.read()
+cap.release()
+out.release()
 
-        if ret:
-            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            cv2.putText(frame, timestamp, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-            cv2.imshow('Video Stream', frame)
-
-            out.write(frame)
-
-        if keyboard.is_pressed('q'):
-            break
-
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
-
-if __name__ == "__main__":
-    output_file = "recorded_video.avi"
-    fps = 20.0
-    duration = 9999999
-
-    camera_index = select_camera()
-    if camera_index is not None:
-        print(f"Using Camera {camera_index + 1}")
-        print("Press 'r' to start recording...")
-        while True:
-            if keyboard.is_pressed('r'):
-                print("Recording started...")
-                record_video(output_file, camera_index, fps, duration)
-                print(f"Video recorded and saved to '{output_file}'.")
-                break
-
-            cv2.waitKey(1)
-
-        print("Press 'q' to exit.")
-        while True:
-            if keyboard.is_pressed('q'):
-                break
+cv2.destroyAllWindows()
