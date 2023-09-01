1ab446f38b10e8c7fcbbb13e1686387f68409101
ishajais <126967483+ishajais@users.noreply.github.com>
2023-09-01 11:31:19 +0530

    Update recording.py

--- recording.py
+++ recording.py
@@ -1,10 +1,18 @@
 import cv2
 import datetime
+import os
+
+def record(frame):
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+    cv2.putText(frame, current_time, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    return frame
 
 cap = cv2.VideoCapture(0)
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID')  
-output_file = 'recorded_video.avi'
+output_folder = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+os.makedirs(output_folder, exist_ok=True)
+output_file = os.path.join(output_folder, f'{output_folder}.avi')
 fps = 30.0  
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -15,13 +23,11 @@ while cap.isOpened():
     if not ret:
         break
 
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    processed_frame = record(frame)
+    
+    cv2.imshow('Surveillance', processed_frame)
 
-    cv2.putText(frame, current_time, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-    cv2.imshow('Surveillance', frame)
-
-    out.write(frame)
+    out.write(processed_frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
